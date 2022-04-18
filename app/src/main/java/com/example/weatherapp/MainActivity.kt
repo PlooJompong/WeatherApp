66fbddfb4303a94a27b511a7b6e70832b262d9db
ponjom2 <ponjom2@hotmail.com>
2022-04-18 21:32:13 +0200

    UI v.1
    Finish the UI v.1

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.location.LocationRequestCompat
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    override fun onCreate (savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView (R.layout.activity_main)
     }
 }
